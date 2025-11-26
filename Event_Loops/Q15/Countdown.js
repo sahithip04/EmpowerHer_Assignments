fb64a8a575eae3ffda9f2c95a5438c4cc76ab7c2
sahithip04 <palparthisahithireddy@gmail.com>
2025-11-26 23:41:33 +0530

    changes done

--- Event_Loops/Q15/Countdown.js
+++ Event_Loops/Q15/Countdown.js
@@ -6,16 +6,18 @@ const rl = readline.createInterface({
 
 rl.question("Enter seconds: ", (input) => {
   let time = Number(input);
+
   const timer = setInterval(() => {
     console.log("Time left:", time);
     time--;
-    
+
     if (time < 0) {
       clearInterval(timer);
       console.log("Countdown Complete!");
       rl.close();
     }
   }, 1000);
+
   function checkKey() {
     setTimeout(() => {
       rl.input.once("data", () => {
